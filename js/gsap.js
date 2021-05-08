1ca7760450aca241ac5cd8716ee17f503285c1b1
Jason Nutt <jnutt367@gmail.com>
2021-05-08 12:17:53 -0400

    commit for eyeballs retreat

--- js/gsap.js
+++ js/gsap.js
@@ -6,3 +6,7 @@ gsap.to("#Tentacle_2", { duration: 4, y: 30, x: 10, ease: "elastic.out", repeat:
 gsap.to("#Tentacle_3", { duration: 3.5, y: 10, x: 10, ease: "elastic.out", repeat: -1 }, 5);
 gsap.to("#Tentacle_4", { duration: 4, y: 10, x: 10, ease: "elastic.out", repeat: -1 }, 5);
 gsap.to("#Tentacle_5", { duration: 6, ease: "elastic.out", y: -10, repeat: -1 }, 5);
+
+gsap.to("#Eye_Stalk_3", { x: -50, y: 20 }, 1)
+gsap.to("#Eye_Stalk_2", { x: -50, y: 70 }, 1)
+gsap.to("#Eye_Stalk_1", { x: 0, y: 50 }, 1)
