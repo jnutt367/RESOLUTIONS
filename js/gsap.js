1370124ae4aa7a11c655b35913a3fe66005924a3
Jason Nutt <jnutt367@gmail.com>
2021-05-08 12:05:50 -0400

    commit re-ordered magic first then tentacles

--- js/gsap.js
+++ js/gsap.js
@@ -1,7 +1,8 @@
-gsap.to("#Tentacle_1", { duration: 3, y: 30, x: 10, ease: "elastic.out", repeat: -1 }, 0);
-gsap.to("#Tentacle_2", { duration: 4, y: 30, x: 10, ease: "elastic.out", repeat: -1 }, 0);
-gsap.to("#Tentacle_3", { duration: 3.5, y: 10, x: 10, ease: "elastic.out", repeat: -1 }, 0);
-gsap.to("#Tentacle_4", { duration: 4, y: 10, x: 10, ease: "elastic.out", repeat: -1 }), 0;
-gsap.to("#Tentacle_5", { duration: 6, ease: "elastic.out", y: -10, repeat: -1 }, 0);
 gsap.to("#Control_Ring", { duration: 3, rotate: 360, ease: "bounce.out" });
-gsap.fromTo("#Magic_Symbols", { opacity: 0 }, { ease: "bounce", x: -30, y: -30, scale: 1.1, duration: 10, opacity: 1 });
+gsap.fromTo("#Magic_Symbols", { opacity: 0 }, { ease: "elastic.inOut", x: -50, y: -30, scale: 1.1, duration: 5, opacity: 1 });
+
+gsap.to("#Tentacle_1", { duration: 3, y: 30, x: 10, ease: "elastic.out", repeat: -1 }, 5);
+gsap.to("#Tentacle_2", { duration: 4, y: 30, x: 10, ease: "elastic.out", repeat: -1 }, 5);
+gsap.to("#Tentacle_3", { duration: 3.5, y: 10, x: 10, ease: "elastic.out", repeat: -1 }, 5);
+gsap.to("#Tentacle_4", { duration: 4, y: 10, x: 10, ease: "elastic.out", repeat: -1 }, 5);
+gsap.to("#Tentacle_5", { duration: 6, ease: "elastic.out", y: -10, repeat: -1 }, 5);
