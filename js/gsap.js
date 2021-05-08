0af9c01d377a796a39e2947c6ac70793000aa98c
Jason Nutt <jnutt367@gmail.com>
2021-05-08 14:33:07 -0400

    commit this new slot lever action yay

--- js/gsap.js
+++ js/gsap.js
@@ -6,7 +6,7 @@ gsap.to("#Tentacle_2", { duration: 4, y: 30, x: 10, ease: "elastic.out", repeat:
 gsap.to("#Tentacle_3", { duration: 3.5, y: 10, x: 10, ease: "elastic.out", repeat: -1 }, 5);
 gsap.to("#Tentacle_4", { duration: 4, y: 10, x: 10, ease: "elastic.out", repeat: -1 }, 5);
 gsap.to("#Tentacle_5", { duration: 6, ease: "elastic.out", y: -10, repeat: -1 }, 5);
-gsap.to("#Clouds_Back", { duration: .3, ease: "bounce", scale: 1.1, x: -10, y: -10, repeat: -1 })
+
 gsap.to("#Eye_Stalk_3", { x: -50, y: 20 }, 1)
 gsap.to("#Eye_Stalk_2", { x: -50, y: 70 }, 1)
 gsap.to("#Eye_Stalk_1", { x: 0, y: 50 }, 1)
@@ -15,4 +15,9 @@ gsap.to("#Small_Box_Jet", { y: 30 }, 2)
 gsap.to("#Tumblers", { stroke: "#e4196e;" }) //fill:#e4196e;
 gsap.to("#lever5", { y: -10, duration: 4, repeat: 2 })
 gsap.to("#lever3", { y: 10, duration: 2, repeat: 3 })
-gsap.to("#lever4", { y: -20, duration: 2 })
+gsap.to("#lever4", { y: -20, duration: 2 });
+
+let cloudsMove = gsap.timeline()
+
+.fromTo("#Clouds_Back", { display: "none" }, { display: "grid", duration: .3, opacity: 1, ease: "elastic", scale: 1.1, x: -3, y: -6, repeat: -1 }, 6)
+    .to("#Slot_Lever", { x: 30, y: 40, rotate: 45 }, 2)
