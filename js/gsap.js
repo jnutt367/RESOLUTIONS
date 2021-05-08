3629d6b4ff9c55f3a73a7d605968e0d2e92cafeb
Jason Nutt <jnutt367@gmail.com>
2021-05-08 11:13:19 -0400

    commit first draft wizard resolutions yay

---
+++ js/gsap.js
@@ -0,0 +1,5 @@
+gsap.to("#Tentacle_1", { duration: 3, y: 100, x: 10 });
+gsap.to("#Tentacle_2", { duration: 2, x: 10, repeat: -1 });
+gsap.to("#Tentacle_3", { duration: 2.5, x: 10, repeat: -1 });
+gsap.to("#Tentacle_4", { duration: 2, x: 10, repeat: -1 });
+gsap.to("#Tentacle_5", { duration: 3, x: 10, repeat: -1 });
