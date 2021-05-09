5c4cf7f2c7f0ee585b8420f5365fda87e450e7ff
Jason Nutt <jnutt367@gmail.com>
2021-05-09 16:32:58 -0400

    commit for eyeball

--- js/gsap.js
+++ js/gsap.js
@@ -67,8 +67,8 @@ let wizardTalk = gsap.timeline()
 
 
 let eyeballsLook = gsap.timeline()
-
-.to("#pupil", { duration: 1, x: -90, y: 48, ease: "bounce.in" }, 5)
+    .to("#eyeball", { scale: 1.005, y: -10, ease: "bounce.out" }, 5.5)
+    .to("#pupil", { duration: 1, x: -90, y: 48, ease: "bounce.in" }, 5)
     .to("#iris", { duration: 1, x: -85, y: 50, ease: "bounce.in" }, 5)
     .to("#eye", { duration: 1, x: -69, y: 30, ease: "bounce.in" }, 5)
     .to("#eyeShadow", { duration: 1, x: -10, y: 10, ease: "bounce.in", opacity: 0 }, 5)
