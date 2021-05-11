4d1d28a623d865b72d00ef1884b6ef3b9146c4ce
Jason Nutt <jnutt367@gmail.com>
2021-05-11 14:46:21 -0400

    commit for double bow and tentacles start

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 gsap.to("html", { backgroundColor: "black" })
-gsap.fromTo("svg", { opacity: 0, scale: 0.9 }, { opacity: 1, scale: 1, duration: 5, ease: "elastic" })
+gsap.fromTo("#Layer_6", { opacity: 0 }, { opacity: 0 }, 0)
+gsap.to("#Clouds_Front", { opacity: 0 })
+gsap.to("#Clouds_Back", { opacity: 0 })
+gsap.to("#Wizard_Action_Layer", { opacity: 0 })
+gsap.to("#Magic_Symbols", { opacity: 0 })
+let doubleBoxLift = gsap.timeline(
+
+    )
+    .fromTo("#Tentacles", { y: 500, scale: 0.1, y: 600, x: 100, opacity: 0 }, { x: 0, scale: 1, opacity: 1, duration: 5, y: 0 }, 2)
+    .fromTo("#Tentacle_5", { y: 700, x: 0, scale: 0, opacity: 0 }, { scale: 1, opacity: 0, duration: 5, y: 0 }, 0)
+    .fromTo("#Main_Floater", { y: 500 }, { duration: 3, y: 0, ease: "elastic.inOut" }, 0)
+    .fromTo("#slide", { x: 0 }, { duration: 3, x: 230, ease: "elastic.inOut" }, 0)
+    .fromTo("#Jet", { opacity: 0 }, { opacity: 1 }, 0)
